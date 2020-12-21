31534149363e48a44727806e4c76cb8c1fd6eb6f
esuryadana32 <esuryadana@gmail.com>
2020-12-21 13:08:10 +0800

    Created Model All

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\ReviewController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +18,11 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+
+Route::apiresource('products', ProductController::class);
+
+Route::group(['prefix'=>'products'], function(){
+	Route::apiresource('/{products}/reviews', ReviewController::class);
 });
