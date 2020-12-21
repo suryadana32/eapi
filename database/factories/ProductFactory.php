1fa2f179dcef661211697efbc2088dcfd82b581e
esuryadana32 <esuryadana@gmail.com>
2020-12-21 18:16:12 +0800

    Seeding part of Product and Review

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -4,6 +4,7 @@ namespace Database\Factories;
 
 use App\Models\Product;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
 
 class ProductFactory extends Factory
 {
@@ -22,7 +23,11 @@ class ProductFactory extends Factory
     public function definition()
     {
         return [
-            //
+            'name'=> $this->faker->word,
+            'detail'=> $this->faker->paragraph,
+            'price'=> $this->faker->numberBetween(100,1000),
+            'stock'=> $this->faker->randomDigit,
+            'discount'=> $this->faker->numberBetween(2,40)
         ];
     }
 }
